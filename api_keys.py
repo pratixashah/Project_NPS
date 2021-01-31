2aa39addb11a59be2ec8d93adeb629780b45d00d
Gigi Jones <72949687+GGGOJO@users.noreply.github.com>
2021-01-30 22:03:25 -0800

    Mini Commit

--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,6 @@
 # OpenWeatherMap API Key
-api_key = "sLjpI2aZ1QcDzkjFvF5UHJdKV2yhnkMs2ita0z7n"
+# Pratixa
+#api_key = "sLjpI2aZ1QcDzkjFvF5UHJdKV2yhnkMs2ita0z7n"
+
+# Gigi
+api_key = "RWseg40uem1yZJtIM0AdnXENOSIfnhBjXRY67Tmi"

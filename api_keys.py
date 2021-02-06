dae3ba36e02937349702810339aa686a53cd4ad8
pratixashah <shah.pratixa@gmail.com>
2021-02-06 14:35:29 -0800

    Final Update

--- api_keys.py
+++ api_keys.py
@@ -4,5 +4,3 @@ api_key = "sLjpI2aZ1QcDzkjFvF5UHJdKV2yhnkMs2ita0z7n"
 # OpenWeatherMap API Key
 weather_api_key = "6e53bcedba230a5533a753d72fe7e1c5"
 
-# Google API Key
-g_key = "AIzaSyB0bxO4KR2toKTlEueEfIaG6sfd3ECRzyE"
